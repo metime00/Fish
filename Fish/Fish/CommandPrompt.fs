88d1dab325c4e29d36fdfc5182915107e0542119
Impl0x <alexmflores@gmail.com>
2014-08-12 19:41:21 -0400

    Created a solution for the Fish library. Implemented a module which allows passing commands to an instance of the windows command prompt.

---
+++ Fish/Fish/CommandPrompt.fs
@@ -0,0 +1,35 @@
+﻿namespace Fish
+
+module CommandPrompt =
+    open System
+    open System.Diagnostics
+    open System.IO
+ 
+    /// Allows multiple commands to be piped to a single instance of cmd.exe,
+    /// similar to the way a sequence of commands can be run from a batch file.
+    let runCmd (cmds : string list) =
+        use p = new Process ()
+ 
+        p.StartInfo.UseShellExecute <- false
+        p.StartInfo.RedirectStandardOutput <- true
+        p.StartInfo.RedirectStandardError <- true
+        p.StartInfo.WorkingDirectory <- @"C:\"
+        p.StartInfo.FileName <- Path.Combine (Environment.SystemDirectory, "cmd.exe")
+    
+        p.StartInfo.RedirectStandardInput <- true
+ 
+        let outputErrorHandler (outLine : DataReceivedEventArgs) =
+            Console.WriteLine outLine.Data
+ 
+        p.OutputDataReceived.Add (outputErrorHandler)
+        p.ErrorDataReceived.Add (outputErrorHandler)
+ 
+        p.Start () |> ignore
+        p.BeginOutputReadLine ()
+        p.BeginErrorReadLine ()
+ 
+        cmds 
+        |> List.iter p.StandardInput.WriteLine
+ 
+        p.StandardInput.WriteLine "exit"
+        p.WaitForExit ()
