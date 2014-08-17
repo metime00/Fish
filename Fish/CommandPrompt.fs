c8fa9a430c278bbf684ef374c899e23b423a453c
metime00 <metime00@gmail.com>
2014-08-16 22:23:21 -0400

    made runCmds return either a list of output strings or an error message

--- Fish/CommandPrompt.fs
+++ Fish/CommandPrompt.fs
@@ -2,14 +2,20 @@
 
 module CommandPrompt =
     open System
+    open System.Text
     open System.Diagnostics
     open System.IO
- 
+    open System.Collections.Generic
+    
+    type CommandOutput =
+        | Success of string list
+        | Failure of string
+
     /// Allows multiple commands to be piped to a single instance of cmd.exe,
     /// similar to the way a sequence of commands can be run from a batch file.
     let runCmd startLocation (cmds : string list) =
-        let errerOut = new Collections.Generic.List<string> ()
-        let outOut = new Collections.Generic.List<string> ()
+        let errorString = new StringBuilder ()
+        let outOut = new List<string> ()
 
         use p = new Process ()
  
@@ -18,17 +24,30 @@ module CommandPrompt =
         p.StartInfo.RedirectStandardError <- true
         p.StartInfo.WorkingDirectory <- startLocation
         p.StartInfo.FileName <- Path.Combine (Environment.SystemDirectory, "cmd.exe")
+        p.StartInfo.Arguments <- "/K @echo off"
     
         p.StartInfo.RedirectStandardInput <- true
-        
+
         p.OutputDataReceived.Add (fun x -> outOut.Add x.Data)
-        p.ErrorDataReceived.Add (fun x -> errerOut.Add x.Data)
+
+        //TODOES make it figure out which command ruined everything, make it wait 
+
+        p.ErrorDataReceived.Add (fun x -> errorString.Append x.Data |> ignore)
+        
         p.Start () |> ignore
         p.BeginOutputReadLine ()
         p.BeginErrorReadLine ()
  
-        cmds |> List.iter p.StandardInput.WriteLine
+        for cmd in cmds do
+            match errorString.Length with
+            | 0 -> p.StandardInput.WriteLine cmd
+            | _ -> ()
 
         p.StandardInput.WriteLine "exit"
         p.WaitForExit ()
-        (List.ofSeq outOut, List.ofSeq errerOut)
+
+        //TODOES isolate and remove command inputs here and organize the outputs by command
+
+        match errorString.Length with
+        | 0 -> CommandOutput.Success(List.ofSeq outOut)
+        | _ -> CommandOutput.Failure(errorString.ToString ())
--- FishApp/Program.fs
+++ FishApp/Program.fs
@@ -2,8 +2,9 @@
 open Fish.CommandPrompt
 
 
-let tomp = [ "echo \"steve\" "; "ls"; "cd Windows"] |> runCmd @"C:\"
-fst tomp |> List.iter Console.WriteLine
-snd tomp |> List.iter Console.WriteLine
+let tomp = [ "echo \"steve\" "; "dir /b"; "cd Windows" ] |> runCmd @"C:\"
+match tomp with
+| CommandOutput.Success outputs -> List.iter (printfn "%s") outputs
+| CommandOutput.Failure message -> Console.WriteLine message
 
 Console.ReadLine () |> ignore
